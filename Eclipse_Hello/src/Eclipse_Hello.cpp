0bbeba7c22baececa699cd59db1ab322fc1adf40
lxdnz254 <lxdnz34@gmail.com>
2018-10-24 20:04:23 +1300

    Eclipse working as well

---
+++ Eclipse_Hello/src/Eclipse_Hello.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Eclipse_Hello.cpp
+// Author      : Alex McBride
+// Version     :
+// Copyright   : 2018
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
