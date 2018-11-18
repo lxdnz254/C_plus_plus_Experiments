ed3e9b6a95665496e79b41ff3510493553426631
lxdnz254 <lxdnz34@gmail.com>
2018-11-18 14:25:02 +1300

    Updated stack

--- Stacker/stack.cpp
+++ Stacker/stack.cpp
@@ -1,36 +1,34 @@
 #include <cassert>
+#include <iostream>
 #include "stack.h"
 
-namespace project
+void Stack::reset()
 {
-    Stack::reset()
-    {
-        Stack::m_next = 0;
-        for (int i = 0; i < Stack::s_maxStackLength; ++i)
-            Stack::m_array[i] = 0;
-    }
+    Stack::m_next = 0;
+    for (int i = 0; i < Stack::s_maxStackLength; ++i)
+        Stack::m_array[i] = 0;
+}
 
-    Stack::push(int x)
-    {
-        if (Stack::m_next == Stack::s_maxStackLength)
-            return false;
+bool Stack::push(int x)
+{
+    if (Stack::m_next == Stack::s_maxStackLength)
+        return false;
 
-        Stack::m_array[Stack::m_next++] = x;
-        return true;
-    }
+    Stack::m_array[Stack::m_next++] = x;
+    return true;
+}
 
-    Stack::pop()
-    {
-        assert(Stack::m_next > 0 && "Can not pop empty stack");
+int Stack::pop()
+{
+    assert(Stack::m_next > 0 && "Can not pop empty stack");
 
-        return m_array[--Stack::m_next];
-    }
+    return m_array[--Stack::m_next];
+}
 
-    Stack::print()
-    {
-        std::cout << "( ";
-        for (int i= 0; i < Stack::m_next; ++i)
-            std::cout << Stack::m_array[i] << ' ';
-        std::cout << ")" << '\n';
-    }
+void Stack::print()
+{
+    std::cout << "( ";
+    for (int i= 0; i < Stack::m_next; ++i)
+        std::cout << Stack::m_array[i] << ' ';
+    std::cout << ")" << '\n';
 }
--- Stacker/stack.h
+++ Stacker/stack.h
@@ -1,22 +1,19 @@
 #ifndef STACK_H
 #define STACK_H
 
-namespace project
+class Stack
 {
-    class Stack
-    {
-        public:
-            void reset();
-            bool push(int x);
-            int pop();
-            void print();
-        protected:
+    public:
+        void reset();
+        bool push(int x);
+        int pop();
+        void print();
+    protected:
 
-        private:
-            static const int s_maxStackLength { 10 };
-            int m_array[s_maxStackLength];
-            int m_next;
-    };
-}
+    private:
+        static const int s_maxStackLength { 10 };
+        int m_array[s_maxStackLength];
+        int m_next;
+};
 
 #endif // STACK_H
